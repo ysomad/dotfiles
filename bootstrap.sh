c00a86f3fa591af44a1c7abb0b7f9cf8f23f4b09
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-05-06 19:52:41 +0700

    removed unes cd

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,7 +23,6 @@ bootstrap_macos () {
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     # oh-my-zsh plugins
-    cd ~/.oh-my-zsh/plugins
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
