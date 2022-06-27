22b83d987f04b24c1aa7c3dc6e48d4d71f3814ba
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-06-28 03:01:01 +0700

    nvim: refactored autocommands

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -43,17 +43,17 @@ autocmd({'BufWritePre'}, {
   pattern = '*.go',
   callback = function()
     local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
-		params.context = {only = {"source.organizeImports"}}
-		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-		for _, res in pairs(result or {}) do
-			for _, r in pairs(res.result or {}) do
-				if r.edit then
-					vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
-				else
-					vim.lsp.buf.execute_command(r.command)
-				end
-			end
-		end
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
 	end,
 })
 
