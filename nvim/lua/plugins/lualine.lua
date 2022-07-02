6f81876c8afa9c016be2bf881391e9214edb2478
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-07-02 15:17:28 +0700

    nvim: lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,26 +1,21 @@
 require('lualine').setup {
   options = {
-    icons_enabled = false,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    always_divide_middle = true,
-    globalstatus = false,
+    globalstatus = true,
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      'NvimTree',
+      'TelescopePrompt',
+      'NeogitStatus',
+      'packer',
+    },
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
+    lualine_a = { 'filename' },
+    lualine_b = { 'branch' },
+    lualine_c = {},
+    lualine_x = { 'diff', { 'diagnostics', sources = { 'nvim_diagnostic' } } },
+    lualine_y = { 'encoding' } ,
+    lualine_z = {},
   },
 }
