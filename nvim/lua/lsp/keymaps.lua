9610f37362642c6fc5093897ff0cd875326b2c20
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-07-10 05:58:54 +0700

    nvim: was bad idea to more languages to separate pkgs

--- nvim/lua/lsp/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-
-local map = vim.keymap.set
-local opts = { noremap=true, silent=true }
-
-map('n', '<leader>e', vim.diagnostic.open_float, opts)
-map('n', '[d', vim.diagnostic.goto_prev, opts)
-map('n', ']d', vim.diagnostic.goto_next, opts)
-map('n', '<leader>q', vim.diagnostic.setloclist, opts)
