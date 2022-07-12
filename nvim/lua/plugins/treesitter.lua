d545cd1872fe96980763a366a183104a2c5b6a4d
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-07-12 19:28:38 +0700

    treesitter: updated cfg

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'lua', 'go', 'bash', 'css', 'gomod', 'gowork', 'html', 'http', 'json',
-    'json5', 'make', 'markdown', 'proto', 'regex', 'scss', 'svelte', 'toml', 'typescript',
-    'vim', 'yaml', 'python', 'javascript' },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = true,
-  },
+  ensure_installed = 'all',
+  ignore_install = { 'phpdoc' },
+  highlight = { enable = true }
 }
