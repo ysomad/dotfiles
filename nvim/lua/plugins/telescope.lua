064875b4c31672ba2770b15ce9cd732ddc896389
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-07-22 16:18:51 +0700

    fixed identation

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,12 @@
 require('telescope').setup{
   defaults = {
-		file_sorter = require('telescope.sorters').get_fzy_sorter,
-		prompt_prefix = '> ',
-		color_devicons = true,
+    file_sorter = require('telescope.sorters').get_fzy_sorter,
+    prompt_prefix = '> ',
+    color_devicons = true,
 
-		file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-		grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-		qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
   },
   pickers = {
     buffers = {
