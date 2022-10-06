0eb9cf4da5b428f34633ce31de3104a8c9bc4969
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-10-07 00:42:18 +0700

    yabai gaps, alacritty cleanup, nvim colorizer background rule

--- nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/lua/plugins/nvim-colorizer.lua
@@ -7,5 +7,5 @@ require('colorizer').setup({'*';}, {
   hsl_fn   = true;         -- CSS hsl() and hsla() functions
   css      = true;         -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
   css_fn   = true;         -- Enable all CSS *functions*: rgb_fn, hsl_fn
-  mode = 'foreground';
+  mode = 'background';
 })
