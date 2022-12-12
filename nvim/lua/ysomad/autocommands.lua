46928f66aeb95c26e84ead3c7b0ad98710b52b66
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-12-12 17:29:12 +0700

    removed old nvim conf

--- nvim/lua/ysomad/autocommands.lua
+++ nvim/lua/ysomad/autocommands.lua
@@ -18,7 +18,7 @@ autocmd('TextYankPost', {
 
 --- remove all trailing whitespace on save
 autocmd('BufWritePre', {
-	group = ysomad_group,
+  group = ysomad_group,
   pattern = '*',
   command = '%s/\\s\\+$//e',
 })
@@ -39,5 +39,29 @@ autocmd('FileType', {
   callback = function()
     vim.opt_local.shiftwidth = 2
     vim.opt_local.tabstop = 2
+  end
+})
+
+-- go: format and organize imports on save
+autocmd('BufWritePre', {
+  pattern = { '*.go' },
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+
+    for _, client in pairs(vim.lsp.buf_get_clients()) do
+      local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
+      params.context = { only = { "source.organizeImports" } }
+
+      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 5000)
+      for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+          if r.edit then
+            vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+          else
+            vim.lsp.buf.execute_command(r.command)
+          end
+        end
+      end
+    end
   end
 })
--- nvim_old/lua/plugins/treesitter.lua
+++
@@ -1,5 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'go', 'lua', 'python', 'make', 'yaml', 'sql', 'proto', 'markdown',
-    'gomod', 'gowork', 'regex', 'toml' },
-  highlight = { enable = true },
-}
