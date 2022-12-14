36237fc3638f48b9b0c4dd64e61966264387a48a
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-12-14 21:45:21 +0700

    lualine and treesitter settings

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,11 +1,18 @@
 require('lualine').setup {
   options = {
+    icons_enabled = false,
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diagnostics'},
+    lualine_b = {
+      'branch',
+      {
+        'diagnostics',
+        symbols = {error = 'E ', warn = 'W ', info = 'I ', hint = 'H '},
+      }
+    },
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
