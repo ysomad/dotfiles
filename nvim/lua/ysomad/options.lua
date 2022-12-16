8ab0aeebea5bb8b4fd8ff47885797043b37fa586
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-12-16 23:27:37 +0700

    update keymaps

--- nvim/lua/ysomad/options.lua
+++ nvim/lua/ysomad/options.lua
@@ -13,7 +13,6 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
-vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
