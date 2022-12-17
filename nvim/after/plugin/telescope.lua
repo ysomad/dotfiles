7c03c95326577fc77c989ce82a3bc60fa79347f3
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-12-17 16:01:39 +0700

    undotree, little fixes

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,5 +4,5 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files)
 vim.keymap.set('n', '<C-p>', builtin.git_files)
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
 vim.keymap.set('n', '<leader>fg', function()
-	builtin.grep_string({ search = vim.fn.input('Grep > ') });
+  builtin.grep_string({ search = vim.fn.input('Grep > ') });
 end)
--- nvim/lua/ysomad/packer.lua
+++ nvim/lua/ysomad/packer.lua
@@ -5,15 +5,14 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
+  -- themes
   -- use 'gruvbox-community/gruvbox'
+
   -- use 'joshdick/onedark.vim'
-  use({
-    'aktersnurra/no-clown-fiesta.nvim',
-    config = function() vim.cmd.colorscheme('no-clown-fiesta') end
-  })
+
   -- use({
   --   'nyoom-engineering/oxocarbon.nvim',
   --   config = function()
@@ -22,10 +21,15 @@ return require('packer').startup(function(use)
   --   end
   -- })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use({
+    'aktersnurra/no-clown-fiesta.nvim',
+    config = function() vim.cmd.colorscheme('no-clown-fiesta') end
+  })
 
+  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
   use('tpope/vim-fugitive')
   use('ray-x/lsp_signature.nvim')
+  use('mbbill/undotree')
 
   use {
     'nvim-lualine/lualine.nvim',
@@ -35,9 +39,9 @@ return require('packer').startup(function(use)
   use {
     'numToStr/Comment.nvim',
     config = function()
-        require('Comment').setup()
+      require('Comment').setup()
     end
-}
+  }
 
   use('lukas-reineke/indent-blankline.nvim')
 
@@ -45,21 +49,21 @@ return require('packer').startup(function(use)
     'VonHeikemen/lsp-zero.nvim',
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
     }
   }
 
