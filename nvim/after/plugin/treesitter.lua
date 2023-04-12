653745b61f2a8d154fee39f93f24c7fbe004e921
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-04-12 18:07:36 +0700

    treesitter parser renamed from help to vimdoc

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'help', 'vim', 'go', 'lua', 'python', 'sql', 'javascript', 'typescript' },
+  ensure_installed = { 'vimdoc', 'vim', 'go', 'lua', 'python', 'sql', 'javascript', 'typescript' },
   sync_install = false,
   auto_install = false,
   highlight = {
