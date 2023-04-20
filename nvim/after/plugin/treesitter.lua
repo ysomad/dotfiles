b7eab3abafeaaa7a89f87efc77d36eaeaf655ad5
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-04-20 15:56:58 +0700

    nvim as submodule 2

--- nvim/after/plugin/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'vimdoc', 'vim', 'go', 'lua', 'python', 'sql', 'javascript', 'typescript' },
-  sync_install = false,
-  auto_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
