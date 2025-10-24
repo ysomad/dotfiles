9fc9231e6d3e1d52eb537c0a469c8405f3657798
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2025-10-25 00:08:35 +0700

    update

--- nix/home.nix
+++ nix/home.nix
@@ -25,6 +25,7 @@
   # plain files is through 'home.file'.
   home.file = {
     ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim";
+    ".config/ghostty".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/ghostty";
   };
 
   # Home Manager can also manage your environment variables through
@@ -125,15 +126,6 @@
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
-    installVimSyntax = true;
-    settings = {
-      font-size = 14;
-      font-family = "BlexMono Nerd Font";
-      cursor-style = "block";
-      background = "151515";
-      foreground = "e1e1e1";
-      window-decoration = false;
-    };
   };
 
   programs.tmux = {
@@ -181,97 +173,45 @@
   programs.kitty.enable = true;
   wayland.windowManager.hyprland = {
     enable = true;
-    # xwayland.enable = true;
+    xwayland.enable = true;
     settings = {
-      # Monitors
       monitor = "DP-1,1920x1080@60,0x0,1";
 
-      # Programs
-      "$terminal" = "kitty";
-      "$fileManager" = "dolphin";
-      "$menu" = "wofi --show drun";
+      "$terminal" = "ghostty";
+      "$fileManager" = "nautilus";
+      "$menu" = "bemenu";
+      "$browser" = "firefox-beta";
 
-      # Autostart
-      # exec-once = [
-      #   "$terminal"
-      #   "nm-applet &"
-      #   "waybar & hyprpaper & firefox"
-      # ];
+      exec-once = [
+        "waybar"
+      ];
 
-      # Environment variables
       env = [
         "XCURSOR_SIZE,24"
         "HYPRCURSOR_SIZE,24"
       ];
 
-      # General settings
       general = {
-        gaps_in = 5;
-        gaps_out = 20;
-        border_size = 2;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
+        gaps_in = 2;
+        gaps_out = 2;
+        border_size = 1;
+        "col.active_border" = "rgba(7e97abee)";
+        "col.inactive_border" = "rgba(373737ee)";
         resize_on_border = false;
         allow_tearing = false;
         layout = "dwindle";
       };
 
-      # Decoration
       decoration = {
-        rounding = 10;
-        rounding_power = 2;
+        rounding = 0;
         active_opacity = 1.0;
         inactive_opacity = 1.0;
-
-        shadow = {
-          enabled = true;
-          range = 4;
-          render_power = 3;
-          color = "rgba(1a1a1aee)";
-        };
-
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-          vibrancy = 0.1696;
-        };
+        shadow.enabled = false;
+        blur.enabled = false;
       };
 
-      # Animations
-      animations = {
-        enabled = true;
+      animations.enabled = false;
 
-        bezier = [
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-          "almostLinear,0.5,0.5,0.75,1"
-          "quick,0.15,0,0.1,1"
-        ];
-
-        animation = [
-          "global,1,10,default"
-          "border,1,5.39,easeOutQuint"
-          "windows,1,4.79,easeOutQuint"
-          "windowsIn,1,4.1,easeOutQuint,popin 87%"
-          "windowsOut,1,1.49,linear,popin 87%"
-          "fadeIn,1,1.73,almostLinear"
-          "fadeOut,1,1.46,almostLinear"
-          "fade,1,3.03,quick"
-          "layers,1,3.81,easeOutQuint"
-          "layersIn,1,4,easeOutQuint,fade"
-          "layersOut,1,1.5,linear,fade"
-          "fadeLayersIn,1,1.79,almostLinear"
-          "fadeLayersOut,1,1.39,almostLinear"
-          "workspaces,1,1.94,almostLinear,fade"
-          "workspacesIn,1,1.21,almostLinear,fade"
-          "workspacesOut,1,1.94,almostLinear,fade"
-          "zoomFactor,1,7,quick"
-        ];
-      };
-
-      # Layout settings
       dwindle = {
         pseudotile = true;
         preserve_split = true;
@@ -281,15 +221,13 @@
         new_status = "master";
       };
 
-      # Misc settings
       misc = {
         force_default_wallpaper = -1;
         disable_hyprland_logo = false;
       };
 
-      # Input settings
       input = {
-        kb_layout = "us";
+        kb_layout = "us,ru";
         kb_variant = "";
         kb_model = "";
         kb_options = "";
@@ -302,35 +240,26 @@
         };
       };
 
-      # Gestures
-      gesture = "3,horizontal,workspace";
-
-      # Per-device config
-      device = {
-        name = "epic-mouse-v1";
-        sensitivity = -0.5;
-      };
-
-      # Main modifier
       "$mainMod" = "SUPER";
 
-      # Keybindings
       bind = [
         # Basic binds
-        "$mainMod, Q, exec, $terminal"
-        "$mainMod, C, killactive,"
+        "$mainMod, RETURN, exec, $terminal"
+        "$mainMod, B, exec, $browser"
+        "$mainMod SHIFT, Q, killactive,"
         "$mainMod, M, exit,"
         "$mainMod, E, exec, $fileManager"
         "$mainMod, V, togglefloating,"
-        "$mainMod, R, exec, $menu"
+        "$mainMod, F, fullscreen,"
+        "$mainMod, D, exec, $menu"
         "$mainMod, P, pseudo," # dwindle
         "$mainMod, J, togglesplit," # dwindle
 
         # Move focus with arrow keys
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
+        "$mainMod, H, movefocus, l"
+        "$mainMod, L, movefocus, r"
+        "$mainMod, K, movefocus, u"
+        "$mainMod, J, movefocus, d"
 
         # Switch workspaces
         "$mainMod, 1, workspace, 1"
@@ -365,13 +294,11 @@
         "$mainMod, mouse_up, workspace, e-1"
       ];
 
-      # Mouse bindings
       bindm = [
         "$mainMod, mouse:272, movewindow"
         "$mainMod, mouse:273, resizewindow"
       ];
 
-      # Laptop multimedia keys
       bindel = [
         ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
         ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
@@ -381,7 +308,6 @@
         ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
       ];
 
-      # Media keys (requires playerctl)
       bindl = [
         ", XF86AudioNext, exec, playerctl next"
         ", XF86AudioPause, exec, playerctl play-pause"
@@ -389,11 +315,23 @@
         ", XF86AudioPrev, exec, playerctl previous"
       ];
 
-      # Window rules
       windowrule = [
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
+    };
+  };
+
+  programs.waybar = {
+    enable = true;
+    settings.main = {
+      modules-center = ["clock"];
+      "clock" = {
+        "interval" = 1;
+        "format" = "{:%A %I:%M}";
+      };
+
+      modules-right = ["cpu" "memory"];
     };
   };
 }
