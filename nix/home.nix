801814fe5d2ab6c68d46ed6ab9b309a285e08508
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2025-11-03 14:10:05 +0700

    update

--- nix/home.nix
+++ nix/home.nix
@@ -16,7 +16,6 @@
     ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim";
     ".config/hypr".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/hypr";
     ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/waybar";
-    ".config/mpv".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/mpv";
   };
 
   programs.home-manager.enable = true;
