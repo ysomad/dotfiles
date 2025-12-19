1e462f6c0ef8b7d6901d964eb377fc44ca282621
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2025-12-19 20:17:00 +0700

    update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -469,6 +469,22 @@
     texlive.combined.scheme-full
   ];
 
+  # steam
+  # https://nixos.wiki/wiki/Steam
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true;
+  #   dedicatedServer.openFirewall = true;
+  #   localNetworkGameTransfers.openFirewall = true;
+  # };
+  # nixpkgs.config.allowUnfreePredicate = pkg:
+  #   builtins.elem (lib.getName pkg) [
+  #     "steam"
+  #     "steam-original"
+  #     "steam-unwrapped"
+  #     "steam-run"
+  #   ];
+
   fonts.packages = with pkgs; [
     font-awesome
   ];
