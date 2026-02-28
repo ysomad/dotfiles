b2f0eb7b47d3fc1e3d2d6f832a91635af61e9f24
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2026-02-28 15:30:07 +0700

    added dropbar, removed mini.icons, better plugin folder structure

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,4 +1,11 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {
+		html = { enabled = false },
+		latex = { enable = false },
+	},
 }
