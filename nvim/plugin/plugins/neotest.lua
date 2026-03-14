8188631eb2929e7c7eeebdb8fac8a57eff173562
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2026-03-14 17:22:23 +0700

    fix: treesitter

--- nvim/plugin/plugins/neotest.lua
+++ nvim/plugin/plugins/neotest.lua
@@ -7,65 +7,82 @@ vim.pack.add({
 	"https://github.com/nvim-neotest/neotest",
 })
 
-local neotest = require("neotest")
-local neotest_golang_opts = {
-	go_test_args = {
-		"-v",
-		"-count=1",
-		"-timeout=60s",
-		"-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
-	},
-	testify_enabled = true,
-}
+vim.api.nvim_create_autocmd("VimEnter", {
+	once = true,
+	callback = function()
+		local neotest = require("neotest")
+		local neotest_golang_opts = {
+			go_test_args = {
+				"-v",
+				"-count=1",
+				"-timeout=60s",
+				"-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+			},
+			testify_enabled = true,
+		}
 
-neotest.setup({
-	adapters = {
-		require("neotest-golang")(neotest_golang_opts),
-	},
-})
+		neotest.setup({
+			adapters = {
+				require("neotest-golang")(neotest_golang_opts),
+			},
+		})
 
-require("coverage").setup({
-	highlights = {
-		covered = { fg = "#90a959" },
-		uncovered = { fg = "#cc6666" },
-	},
+		require("coverage").setup({
+			highlights = {
+				covered = { fg = "#90a959" },
+				uncovered = { fg = "#cc6666" },
+			},
+		})
+	end,
 })
 
 vim.keymap.set("n", "<leader>tn", function()
-	neotest.run.run()
+	require("neotest").run.run()
 end, { desc = "[t]est [n]earest" })
+
 vim.keymap.set("n", "<leader>tf", function()
-	neotest.run.run(vim.fn.expand("%"))
+	require("neotest").run.run(vim.fn.expand("%"))
 end, { desc = "[t]est run [f]ile" })
+
 vim.keymap.set("n", "<leader>tA", function()
-	neotest.run.run(vim.uv.cwd())
+	require("neotest").run.run(vim.uv.cwd())
 end, { desc = "[t]est [A]ll files" })
+
 vim.keymap.set("n", "<leader>tt", function()
-	neotest.run.stop()
+	require("neotest").run.stop()
 end, { desc = "[t]est [t]erminate" })
+
 vim.keymap.set("n", "<leader>to", function()
-	neotest.output.open({ enter = true, auto_close = true })
+	require("neotest").output.open({ enter = true, auto_close = true })
 end, { desc = "[t]est [o]output" })
+
 vim.keymap.set("n", "<leader>tO", function()
-	neotest.output_panel.toggle()
+	require("neotest").output_panel.toggle()
 end, { desc = "[t]est [O]output" })
+
 vim.keymap.set("n", "<leader>ts", function()
-	neotest.summary.toggle()
+	require("neotest").summary.toggle()
 end, { desc = "[t]est [s]ummary" })
+
 vim.keymap.set("n", "<leader>tw", function()
-	neotest.watch.toggle()
+	require("neotest").watch.toggle()
 end, { desc = "[t]est [w]atch" })
+
 vim.keymap.set("n", "]t", function()
-	neotest.jump.next()
+	require("neotest").jump.next()
 end, { desc = "next test" })
+
 vim.keymap.set("n", "[t", function()
-	neotest.jump.prev()
+	require("neotest").jump.prev()
 end, { desc = "prev test" })
+
 vim.keymap.set("n", "]T", function()
-	neotest.jump.next({ status = "failed" })
+	require("neotest").jump.next({ status = "failed" })
 end, { desc = "next failed test" })
+
 vim.keymap.set("n", "[T", function()
-	neotest.jump.prev({ status = "failed" })
+	require("neotest").jump.prev({ status = "failed" })
 end, { desc = "prev failed test" })
+
 vim.keymap.set("n", "<leader>tc", "<cmd>Coverage<cr>", { desc = "[t]est [c]overage" })
 vim.keymap.set("n", "<leader>tC", "<cmd>CoverageClear<cr>", { desc = "[t]est [C]overage clear" })
