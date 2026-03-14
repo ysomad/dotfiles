9723ae9e820b4de67c4396bef1314cfec37a1f6e
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2026-03-14 17:22:23 +0700

    fix: treesitter

--- nvim/plugin/plugins/fidget.lua
+++ nvim/plugin/plugins/fidget.lua
@@ -1,3 +1,9 @@
 vim.pack.add({
 	"https://github.com/j-hui/fidget.nvim",
 })
+
+require("fidget").setup({
+	notification = {
+		override_vim_notify = true,
+	},
+})
--- nvim/plugin/plugins/treesitter.lua
+++ nvim/plugin/plugins/treesitter.lua
@@ -1,14 +1,12 @@
 vim.api.nvim_create_autocmd("PackChanged", {
 	callback = function(ev)
-		if ev.data.spec.name ~= "nvim-treesitter" then
-			return
+		local name, kind = ev.data.spec.name, ev.data.kind
+		if name == "nvim-treesitter" and kind == "update" then
+			if not ev.data.active then
+				vim.cmd.packadd("nvim-treesitter")
+			end
+			vim.cmd("TSUpdate")
 		end
-
-		if ev.data.kind ~= "install" and ev.data.kind ~= "update" then
-			return
-		end
-
-		vim.cmd.TSUpdate()
 	end,
 })
 
@@ -17,17 +15,41 @@ vim.pack.add({
 		src = "https://github.com/nvim-treesitter/nvim-treesitter",
 		version = "main",
 	},
+}, {
+	load = function(plugin)
+		vim.cmd.packadd(plugin.spec.name)
+		vim.opt.runtimepath:append(plugin.path .. "/runtime")
+	end,
 })
 
 require("nvim-treesitter").install({
+	"bash",
 	"go",
-	"sql",
+	"javascript",
+	"lua",
+	"markdown",
+	"markdown_inline",
 	"python",
 	"rust",
-	"yaml",
-	"bash",
-	"lua",
-	"javascript",
+	"sql",
 	"typescript",
-	"latex",
+	"tsx",
+	"yaml",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"bash",
+		"go",
+		"javascript",
+		"lua",
+		"markdown",
+		"python",
+		"rust",
+		"sh",
+		"typescript",
+	},
+	callback = function(ev)
+		pcall(vim.treesitter.start, ev.buf)
+	end,
 })
