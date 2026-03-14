501cac4d7ab66b627fc8ad176370f0a078a211d4
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2026-03-14 16:28:42 +0700

    ref: better plugin separation

--- nvim/plugin/plugins/lualine.lua
+++ nvim/plugin/plugins/lualine.lua
@@ -1,12 +1,5 @@
-vim.o.timeout = true
-vim.o.timeoutlen = 300
-
 vim.pack.add({
-	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/nvim-lualine/lualine.nvim",
-	"https://github.com/folke/which-key.nvim",
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
 
 require("lualine").setup({
@@ -23,11 +16,4 @@ require("lualine").setup({
 		lualine_y = {},
 		lualine_z = { "location" },
 	},
-})
-
-require("which-key").setup({})
-
-require("render-markdown").setup({
-	html = { enabled = false },
-	latex = { enabled = false },
 })
