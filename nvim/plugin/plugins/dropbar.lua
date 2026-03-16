b9c8a2fe89e013b913ac18da99051044534cd57e
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2026-03-16 12:01:50 +0700

    nvim: dropbar transparent bg

--- nvim/plugin/plugins/dropbar.lua
+++ nvim/plugin/plugins/dropbar.lua
@@ -1,6 +1,16 @@
 vim.pack.add({ "https://github.com/Bekaboo/dropbar.nvim" })
 
+require("dropbar").setup()
+
 local api = require("dropbar.api")
+
+vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DropBarIconUISeparator", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DropBarCurrentContext", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DropBarMenuCurrentContext", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DropBarMenuNormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DropBarMenuFloatBorder", { bg = "NONE" })
 
 vim.keymap.set("n", "<Leader>;", api.pick, { desc = "Pick symbols in winbar" })
 vim.keymap.set("n", "[;", api.goto_context_start, { desc = "Go to start of current context" })
